26ed4e9d1d95bf27c53312573d45b1220a9faaa7
Alessandro-car <carellaalessandro1@gmail.com>
2026-05-21 12:06:12 +0200

    Corretti livelli di visibilità della classe ContinuousAttribute

--- src/data/ContinuousAttribute.java
+++ src/data/ContinuousAttribute.java
@@ -6,7 +6,7 @@ package data;
  * Rappresenta un attributo con valori numerici continui.
  * A differenza degli attributi discreti, {@code ContinuousAttribute} gestisce infiniti range di valori.
  */
-class ContinuousAttribute extends Attribute {
+public class ContinuousAttribute extends Attribute {
 		/**
 		 * È il costruttore di classe.
 		 * @param name Nome simbolico dell'attributo
